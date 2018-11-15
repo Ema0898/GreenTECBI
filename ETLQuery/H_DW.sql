04eb6061bfbb6687f77753022d89ef89532e055d
Emmanuelle Aguilar Sánchez <aguisnchez@gmail.com>
2018-11-15 00:06:03 -0600

    Termina de hacer los querys del ETL y realiza el ETL

--- ETLQuery/H_DW.sql
+++ ETLQuery/H_DW.sql
@@ -14,3 +14,60 @@ FROM Excursion ex
   INNER JOIN Horario h ON he.idHorarioExcursion = h.idHorarioExcursion
 WHERE he.Fecha = v.FechaEntrada
 GROUP BY he.Fecha
+
+
+------ Final ------
+SELECT GastoTotal, IngresoTotal, 
+       ROW_NUMBER() OVER (ORDER BY Fecha) idElementoParque, 
+       ROW_NUMBER() OVER (ORDER BY Fecha) idGasto, 
+	     ROW_NUMBER() OVER (ORDER BY Fecha) idIngreso,
+	     ROW_NUMBER() OVER (ORDER BY Fecha) idTiempo
+FROM
+(
+   SELECT org.PrecioOrganizacion + sal.SalarioEmpleados AS GastoTotal, org.Fecha1 AS Fecha 
+   FROM 
+  (
+     SELECT SUM(e.PrecioOrganizacion) AS PrecioOrganizacion, he.Fecha AS Fecha1
+     FROM Excursion e
+       INNER JOIN HorarioXExcursion he ON he.idExcursion = e.idExcursion
+     GROUP BY he.Fecha
+  ) org 
+
+  INNER JOIN
+
+  (
+     SELECT SUM(pg.Monto) AS SalarioEmpleados , pg.Fecha AS Fecha2
+     FROM Empleado e 
+       INNER JOIN Pago pg ON pg.idEmpleado = e.idEmpleado
+     GROUP BY pg.Fecha
+  ) sal
+  ON org.Fecha1 = sal.Fecha2
+  --ORDER BY org.Fecha1
+) gastos
+INNER JOIN
+(
+
+   SELECT PrecioAlojamiento + PrecioEntradaExcursion AS IngresoTotal, alojamiento.FechaEntrada FROM
+   (
+     SELECT SUM(aj.PrecioAlojamiento) AS PrecioAlojamiento, v.FechaEntrada
+     FROM Alojamiento aj 
+     INNER JOIN Visitante v ON aj.idAlojamiento = v.idAlojamiento
+     GROUP BY v.FechaEntrada
+   ) alojamiento
+   INNER JOIN
+   (
+     SELECT SUM(evento.PrecioEntrada) AS PrecioEntradaExcursion, he.Fecha 
+     FROM EventoExcursion evento
+     INNER JOIN HorarioXExcursion he ON he.idHorarioXExcursion = evento.idHorarioXExcursion
+     GROUP BY he.Fecha
+   )
+   eventoexcursion ON alojamiento.FechaEntrada = eventoexcursion.Fecha
+   --ORDER BY eventoexcursion.Fecha
+) ingresos 
+ON gastos.Fecha = ingresos.FechaEntrada
+ORDER BY gastos.Fecha
+
+
+
+
+
